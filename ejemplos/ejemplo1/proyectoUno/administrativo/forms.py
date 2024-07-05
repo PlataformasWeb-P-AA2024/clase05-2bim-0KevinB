ed1977e048c4b427c7892f70c97d147e84fa094a
0KevinB <kabarrazueta@utpl.edu.ec>
2024-07-05 17:11:31 -0500

    Primer commit

--- ejemplos/ejemplo1/proyectoUno/administrativo/forms.py
+++ ejemplos/ejemplo1/proyectoUno/administrativo/forms.py
@@ -10,7 +10,7 @@ class EstudianteForm(ModelForm):
         model = Estudiante
         fields = ['nombre', 'apellido', 'cedula', 'correo']
         labels = {
-            'nombre': _('Ingrese nombre por favor'),
+            'nombre': _('Ingrese nombre'),
             'apellido': _('Ingrese apellido por favor'),
             'cedula': _('Ingrese cédula por favor'),
             'correo': _('Ingrese correo por favor'),
@@ -46,8 +46,8 @@ class EstudianteForm(ModelForm):
 
     def clean_correo(self):
         valor = self.cleaned_data['correo']
-        if "@" not in valor or "utpl.edu.ec" not in valor:
-            raise forms.ValidationError("Ingrese correo válido para la Universidad")
+        if "@" not in valor or "utpl.edu.ec" not in valor or "gmail.com" not in valor:
+            raise forms.ValidationError("Ingrese correo válido para la Universidad / Gmail")
         return valor
 
 
@@ -55,8 +55,18 @@ class NumeroTelefonicoForm(ModelForm):
     class Meta:
         model = NumeroTelefonico
         fields = ['telefono', 'tipo', 'estudiante']
+    def clean_telefono(self):
+        valor = self.cleaned_data['telefono']
+        if len(valor) != 10:
+            raise forms.ValidationError("Ingrese numero con 10 digitos")
+        return valor
+    def clean_tipo(self):
+        valor = self.cleaned_data['tipo']
+        num_palabras = len(valor.split())
 
-
+        if num_palabras < 2:
+            raise forms.ValidationError("Ingrese dos palabras")
+        return valor
 class NumeroTelefonicoEstudianteForm(ModelForm):
 
     def __init__(self, estudiante, *args, **kwargs):
